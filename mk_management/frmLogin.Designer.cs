08651aa95fb8e3b9af04506c846032130cdf22b0
David Ramos <davidramos015@gmail.com>
2023-04-15 14:15:04 -0600

    feat : login form

---
+++ mk_management/frmLogin.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace mk_management
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DevExpress.XtraEditors.Controls.EditorButtonImageOptions editorButtonImageOptions1 = new DevExpress.XtraEditors.Controls.EditorButtonImageOptions();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            DevExpress.Utils.SerializableAppearanceObject serializableAppearanceObject1 = new DevExpress.Utils.SerializableAppearanceObject();
+            DevExpress.Utils.SerializableAppearanceObject serializableAppearanceObject2 = new DevExpress.Utils.SerializableAppearanceObject();
+            DevExpress.Utils.SerializableAppearanceObject serializableAppearanceObject3 = new DevExpress.Utils.SerializableAppearanceObject();
+            DevExpress.Utils.SerializableAppearanceObject serializableAppearanceObject4 = new DevExpress.Utils.SerializableAppearanceObject();
+            this.dataLayoutControl1 = new DevExpress.XtraDataLayout.DataLayoutControl();
+            this.txtClave = new DevExpress.XtraEditors.ButtonEdit();
+            this.txtUsuario = new DevExpress.XtraEditors.TextEdit();
+            this.btnIniciar = new DevExpress.XtraEditors.SimpleButton();
+            this.btnCerrar = new DevExpress.XtraEditors.SimpleButton();
+            this.imgLogo = new DevExpress.XtraEditors.PictureEdit();
+            this.layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.emptySpaceItem4 = new DevExpress.XtraLayout.EmptySpaceItem();
+            this.emptySpaceItem5 = new DevExpress.XtraLayout.EmptySpaceItem();
+            this.layoutControlItem5 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem4 = new DevExpress.XtraLayout.LayoutControlItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataLayoutControl1)).BeginInit();
+            this.dataLayoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtClave.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtUsuario.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgLogo.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataLayoutControl1
+            // 
+            this.dataLayoutControl1.Controls.Add(this.txtClave);
+            this.dataLayoutControl1.Controls.Add(this.txtUsuario);
+            this.dataLayoutControl1.Controls.Add(this.btnIniciar);
+            this.dataLayoutControl1.Controls.Add(this.btnCerrar);
+            this.dataLayoutControl1.Controls.Add(this.imgLogo);
+            this.dataLayoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataLayoutControl1.Location = new System.Drawing.Point(0, 0);
+            this.dataLayoutControl1.Name = "dataLayoutControl1";
+            this.dataLayoutControl1.Root = this.layoutControlGroup1;
+            this.dataLayoutControl1.Size = new System.Drawing.Size(437, 480);
+            this.dataLayoutControl1.TabIndex = 0;
+            this.dataLayoutControl1.Text = "dataLayoutControl1";
+            // 
+            // txtClave
+            // 
+            this.txtClave.Location = new System.Drawing.Point(92, 351);
+            this.txtClave.Name = "txtClave";
+            this.txtClave.Properties.Appearance.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txtClave.Properties.Appearance.Options.UseFont = true;
+            editorButtonImageOptions1.ImageToTextAlignment = DevExpress.XtraEditors.ImageAlignToText.RightCenter;
+            editorButtonImageOptions1.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("editorButtonImageOptions1.SvgImage")));
+            editorButtonImageOptions1.SvgImageSize = new System.Drawing.Size(20, 20);
+            this.txtClave.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Glyph, "Mostrar / Ocultar", -1, true, true, false, editorButtonImageOptions1, new DevExpress.Utils.KeyShortcut(System.Windows.Forms.Keys.None), serializableAppearanceObject1, serializableAppearanceObject2, serializableAppearanceObject3, serializableAppearanceObject4, "", null, null, DevExpress.Utils.ToolTipAnchor.Default)});
+            this.txtClave.Properties.MaxLength = 200;
+            this.txtClave.Properties.UseSystemPasswordChar = true;
+            this.txtClave.Size = new System.Drawing.Size(313, 28);
+            this.txtClave.StyleController = this.dataLayoutControl1;
+            this.txtClave.TabIndex = 1;
+            this.txtClave.ButtonClick += new DevExpress.XtraEditors.Controls.ButtonPressedEventHandler(this.txtClave_ButtonClick);
+            this.txtClave.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtClave_KeyDown);
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.EnterMoveNextControl = true;
+            this.txtUsuario.Location = new System.Drawing.Point(92, 299);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Properties.Appearance.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txtUsuario.Properties.Appearance.Options.UseFont = true;
+            this.txtUsuario.Properties.MaxLength = 30;
+            this.txtUsuario.Size = new System.Drawing.Size(313, 28);
+            this.txtUsuario.StyleController = this.dataLayoutControl1;
+            this.txtUsuario.TabIndex = 0;
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Appearance.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnIniciar.Appearance.Options.UseFont = true;
+            this.btnIniciar.ImageOptions.ImageToTextAlignment = DevExpress.XtraEditors.ImageAlignToText.TopCenter;
+            this.btnIniciar.ImageOptions.Location = DevExpress.XtraEditors.ImageLocation.MiddleCenter;
+            this.btnIniciar.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btnIniciar.ImageOptions.SvgImage")));
+            this.btnIniciar.Location = new System.Drawing.Point(235, 393);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.ShowFocusRectangle = DevExpress.Utils.DefaultBoolean.True;
+            this.btnIniciar.Size = new System.Drawing.Size(170, 65);
+            this.btnIniciar.StyleController = this.dataLayoutControl1;
+            this.btnIniciar.TabIndex = 3;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Appearance.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnCerrar.Appearance.Options.UseFont = true;
+            this.btnCerrar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCerrar.ImageOptions.ImageToTextAlignment = DevExpress.XtraEditors.ImageAlignToText.TopCenter;
+            this.btnCerrar.ImageOptions.Location = DevExpress.XtraEditors.ImageLocation.MiddleCenter;
+            this.btnCerrar.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btnCerrar.ImageOptions.SvgImage")));
+            this.btnCerrar.Location = new System.Drawing.Point(61, 393);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.ShowFocusRectangle = DevExpress.Utils.DefaultBoolean.True;
+            this.btnCerrar.Size = new System.Drawing.Size(160, 65);
+            this.btnCerrar.StyleController = this.dataLayoutControl1;
+            this.btnCerrar.TabIndex = 2;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // imgLogo
+            // 
+            this.imgLogo.EditValue = ((object)(resources.GetObject("imgLogo.EditValue")));
+            this.imgLogo.Location = new System.Drawing.Point(21, 21);
+            this.imgLogo.Name = "imgLogo";
+            this.imgLogo.Properties.AllowFocused = false;
+            this.imgLogo.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.imgLogo.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
+            this.imgLogo.Properties.ShowMenu = false;
+            this.imgLogo.Properties.ShowZoomSubMenu = DevExpress.Utils.DefaultBoolean.False;
+            this.imgLogo.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Squeeze;
+            this.imgLogo.Size = new System.Drawing.Size(395, 265);
+            this.imgLogo.StyleController = this.dataLayoutControl1;
+            this.imgLogo.TabIndex = 4;
+            this.imgLogo.EditValueChanged += new System.EventHandler(this.imgLogo_EditValueChanged);
+            // 
+            // layoutControlGroup1
+            // 
+            this.layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.layoutControlGroup1.GroupBordersVisible = false;
+            this.layoutControlGroup1.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1,
+            this.layoutControlItem2,
+            this.layoutControlItem3,
+            this.emptySpaceItem4,
+            this.emptySpaceItem5,
+            this.layoutControlItem5,
+            this.layoutControlItem4});
+            this.layoutControlGroup1.Name = "layoutControlGroup1";
+            this.layoutControlGroup1.Padding = new DevExpress.XtraLayout.Utils.Padding(20, 20, 20, 20);
+            this.layoutControlGroup1.Size = new System.Drawing.Size(437, 480);
+            this.layoutControlGroup1.TextVisible = false;
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.imgLogo;
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.MinSize = new System.Drawing.Size(22, 22);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Padding = new DevExpress.XtraLayout.Utils.Padding(1, 1, 1, 1);
+            this.layoutControlItem1.Size = new System.Drawing.Size(397, 267);
+            this.layoutControlItem1.SizeConstraintsType = DevExpress.XtraLayout.SizeConstraintsType.Custom;
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem1.TextVisible = false;
+            // 
+            // layoutControlItem2
+            // 
+            this.layoutControlItem2.Control = this.btnCerrar;
+            this.layoutControlItem2.Location = new System.Drawing.Point(39, 371);
+            this.layoutControlItem2.MaxSize = new System.Drawing.Size(174, 69);
+            this.layoutControlItem2.MinSize = new System.Drawing.Size(174, 69);
+            this.layoutControlItem2.Name = "layoutControlItem2";
+            this.layoutControlItem2.Padding = new DevExpress.XtraLayout.Utils.Padding(2, 12, 2, 2);
+            this.layoutControlItem2.Size = new System.Drawing.Size(174, 69);
+            this.layoutControlItem2.SizeConstraintsType = DevExpress.XtraLayout.SizeConstraintsType.Custom;
+            this.layoutControlItem2.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem2.TextVisible = false;
+            // 
+            // layoutControlItem3
+            // 
+            this.layoutControlItem3.Control = this.btnIniciar;
+            this.layoutControlItem3.Location = new System.Drawing.Point(213, 371);
+            this.layoutControlItem3.MaxSize = new System.Drawing.Size(174, 69);
+            this.layoutControlItem3.MinSize = new System.Drawing.Size(174, 69);
+            this.layoutControlItem3.Name = "layoutControlItem3";
+            this.layoutControlItem3.Size = new System.Drawing.Size(174, 69);
+            this.layoutControlItem3.SizeConstraintsType = DevExpress.XtraLayout.SizeConstraintsType.Custom;
+            this.layoutControlItem3.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem3.TextVisible = false;
+            // 
+            // emptySpaceItem4
+            // 
+            this.emptySpaceItem4.AllowHotTrack = false;
+            this.emptySpaceItem4.Location = new System.Drawing.Point(0, 371);
+            this.emptySpaceItem4.Name = "emptySpaceItem4";
+            this.emptySpaceItem4.Size = new System.Drawing.Size(39, 69);
+            this.emptySpaceItem4.TextSize = new System.Drawing.Size(0, 0);
+            // 
+            // emptySpaceItem5
+            // 
+            this.emptySpaceItem5.AllowHotTrack = false;
+            this.emptySpaceItem5.Location = new System.Drawing.Point(387, 371);
+            this.emptySpaceItem5.Name = "emptySpaceItem5";
+            this.emptySpaceItem5.Size = new System.Drawing.Size(10, 69);
+            this.emptySpaceItem5.TextSize = new System.Drawing.Size(0, 0);
+            // 
+            // layoutControlItem5
+            // 
+            this.layoutControlItem5.AppearanceItemCaption.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.layoutControlItem5.AppearanceItemCaption.Options.UseFont = true;
+            this.layoutControlItem5.Control = this.txtClave;
+            this.layoutControlItem5.Location = new System.Drawing.Point(0, 319);
+            this.layoutControlItem5.Name = "layoutControlItem5";
+            this.layoutControlItem5.Padding = new DevExpress.XtraLayout.Utils.Padding(12, 12, 12, 12);
+            this.layoutControlItem5.Size = new System.Drawing.Size(397, 52);
+            this.layoutControlItem5.Text = "Clave:";
+            this.layoutControlItem5.TextSize = new System.Drawing.Size(57, 21);
+            // 
+            // layoutControlItem4
+            // 
+            this.layoutControlItem4.AppearanceItemCaption.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.layoutControlItem4.AppearanceItemCaption.Options.UseFont = true;
+            this.layoutControlItem4.Control = this.txtUsuario;
+            this.layoutControlItem4.Location = new System.Drawing.Point(0, 267);
+            this.layoutControlItem4.Name = "layoutControlItem4";
+            this.layoutControlItem4.Padding = new DevExpress.XtraLayout.Utils.Padding(12, 12, 12, 12);
+            this.layoutControlItem4.Size = new System.Drawing.Size(397, 52);
+            this.layoutControlItem4.Text = "Usuario:";
+            this.layoutControlItem4.TextSize = new System.Drawing.Size(57, 21);
+            // 
+            // frmLogin
+            // 
+            this.Appearance.BackColor = System.Drawing.Color.White;
+            this.Appearance.Options.UseBackColor = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCerrar;
+            this.ClientSize = new System.Drawing.Size(437, 480);
+            this.Controls.Add(this.dataLayoutControl1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmLogin";
+            this.Opacity = 0.95D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kaz WiFi HotSpot";
+            this.TransparencyKey = System.Drawing.Color.Blue;
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataLayoutControl1)).EndInit();
+            this.dataLayoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.txtClave.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtUsuario.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgLogo.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraDataLayout.DataLayoutControl dataLayoutControl1;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup1;
+        private DevExpress.XtraEditors.SimpleButton btnCerrar;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
+        private DevExpress.XtraEditors.SimpleButton btnIniciar;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem3;
+        private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem4;
+        private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem5;
+        private DevExpress.XtraEditors.TextEdit txtUsuario;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem4;
+        private DevExpress.XtraEditors.ButtonEdit txtClave;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem5;
+        private DevExpress.XtraEditors.PictureEdit imgLogo;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+    }
+}
---
+++ mk_management/frmLogin.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using mk_management.common;
+
+namespace mk_management
+{
+    public partial class frmLogin : DevExpress.XtraEditors.XtraForm
+    {
+        private bool _isLogued = false;
+
+        public frmLogin(Icon icono)
+        {
+            InitializeComponent();
+            this.Icon = icono;
+
+            txtUsuario.Properties.CharacterCasing = CharacterCasing.Upper;
+            txtUsuario.Properties.Mask.MaskType = DevExpress.XtraEditors.Mask.MaskType.RegEx;
+            txtUsuario.Properties.Mask.EditMask = frmAgregarUsuarioSistema.MASCARA_USUARIO;//Letras,numeros y guion bajo
+
+            //txtUsuario.Text = "ADMIN";
+            //txtClave.Text = "mk_admin";
+
+            btnIniciar.Focus();
+        }
+
+
+        public bool LoginSuccessFull { get { return _isLogued; } }
+
+
+        public string UsuarioAutenticado { get { return _isLogued ? txtUsuario.Text : ""; } }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            _isLogued = false;
+            this.Close();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Utilerias.IsNullOrEmpty(txtUsuario.EditValue))
+                    return;
+
+                if (Utilerias.IsNullOrEmpty(txtClave.EditValue))
+                    return;
+
+                var pass = Crypto.Encrypt(txtClave.Text + "|" + txtUsuario.Text);
+
+                var query = $@" select u.Nombre,u.Apellido,u.Id,u.Usuario 
+                                  from usuario_sistema u 
+                                 where u.Usuario = '{txtUsuario.EditValue}' 
+                                   and u.Clave ='{pass}' 
+                                   and u.Activo = 1 ";
+
+                var cnn = ConfigurationSettings.Obtener_CadenaConexion();
+                var dtUser = MDB.FillDataTable(cnn, query);
+
+                if (Utilerias.TablaTieneRows(dtUser))
+                {
+                    var link = ConfigurationSettings.Obtener_CadenaConexion();
+                    var comp = wOverlaySplash.GetCompanyInformation(link);
+
+                    Modulo.IdAplicacion = "R2019-03";
+                    Modulo.TokenAp = "EK2Swy0YXX4ZkNraqG5ZXHfsK1wog0k8";
+
+                    Modulo.IdUsuario = Utilerias.SafeToString(dtUser.Rows[0]["Usuario"]);
+                    Modulo.Usuario = Utilerias.SafeToString(dtUser.Rows[0]["Usuario"]);
+                    Modulo.NombreUsuario = Utilerias.SafeToString(dtUser.Rows[0]["Nombre"]) + " " +
+                                           Utilerias.SafeToString(dtUser.Rows[0]["Apellido"]);
+
+
+                    Modulo.NombreEmpresa = comp.Nombre;
+                    common.Link.SetData(cnn, comp.Nombre, Modulo.IdAplicacion, Modulo.IdUsuario);
+                    hotspot.Link.SetData(cnn, comp.Nombre, Modulo.IdAplicacion, Modulo.IdUsuario);
+
+                    _isLogued = true;
+                    this.Close();
+                }
+                else
+                {
+                    txtClave.Focus();
+                    Utilerias.msjInfo("Usuario o clave incorrecta.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Utilerias.msjErrorEx(ex);
+            }
+        }
+
+        private void txtClave_ButtonClick(object sender, DevExpress.XtraEditors.Controls.ButtonPressedEventArgs e)
+        {
+            txtClave.Properties.UseSystemPasswordChar = !txtClave.Properties.UseSystemPasswordChar;
+
+            if (txtClave.Properties.Buttons.Count > 0)
+                txtClave.Properties.Buttons[0].Caption = txtClave.Properties.UseSystemPasswordChar ? "Mostrar" : "Ocultar";
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+            txtClave.Properties.UseSystemPasswordChar = false;//hacer el cambio al llamar al evento
+            txtClave_ButtonClick(null, null);
+            btnIniciar.Focus();
+        }
+
+        private void txtClave_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnIniciar.Focus();
+                this.Refresh();
+                btnIniciar.PerformClick();
+            }
+
+        }
+
+        private void imgLogo_EditValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
